97bd580068880610ff528f3baa407ee4b962ba43
florianfouchard <florianrm.fouchard@gmail.com>
2016-10-24 14:56:04 +0200

    substr et substring

--- src/types/strings.js
+++ src/types/strings.js
@@ -43,5 +43,5 @@ console.log('y :', y);
 
 //substring
 var string = '01234567899';
-console.log('substr(3,6) :', string.substr(3,6), 'extrait une chaine de 6 caractère à partir de la 3eme position');
-console.log('substring(3,6) :', string.substring(3,6), 'extrait la chaine comprise entre les positions 3 et 6');
+console.log('substr(3,6) :', string.substr(3,6), 'extract a string from the third position with a length of 6');
+console.log('substring(3,6) :', string.substring(3,6), 'extract a string from the third position to the 6th');
