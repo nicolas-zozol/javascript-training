a12147396775ab6f2fa6ddcc08e654cfd1dc56b6
romain <romain.trouillard1@gmail.com>
2016-10-24 15:11:07 +0200

    substringromain.

---
+++ src/types/number.js
@@ -0,0 +1,18 @@
+var x = 2;
+console.log(typeof x);
+
+x= x+0.2;
+console.log('x : ', x, typeof x);
+
+x= parseInt("4.2"); //recu^père valeur numérique
+console.log('x : ', x, typeof x);
+
+var y= parseFloat('2.345');
+console.log('y : ', y, typeof y);
+
+var z= parseFloat('ah ah ah');
+console.log('z : ', z, typeof z);
+
+
+var z= parseFloat('ah ah ah');
+console.log('z : ', z, ' au fromage hummmmmmm');
