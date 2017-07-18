7f8c5ef879d6eb2f3ed729b5ba39aa3b63c27048
Nicolas Zozol <nzozol@gmail.com>
2017-07-18 21:11:31 +0200

    Better build

--- js-build/src/program/creation.js
+++ js-build/src/program/creation.js
@@ -1,7 +1,9 @@
 //creation.js
-const Carnivore = require('../classes/carnivore');
+const Carnivore = require('../classes/carnivore')
 const Dinosaurus = require('../classes/dinosaurus');
-const _ = require('underscore');
+
+// const _ = require('underscore');
+
 
 const tRex = new Carnivore('TRex');
 const serra = new Carnivore('serra');
@@ -10,15 +12,23 @@ const gentil = new Carnivore('TRex');
 gentil.nice = true;
 
 const carnivores = [tRex, serra, gentil];
+console.log(carnivores);
 
+
+/*
 const group = _.groupBy(carnivores, 'name');
 
 console.log('group : ', group);
 
 tRex.eat({name : 'Lapin'});
 
+
+
+
+
 module.exports={
     Carnivore : Carnivore,
     Dinosaurus : Dinosaurus,
     gentil : gentil
 };
+*/
